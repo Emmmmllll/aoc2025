95daf95b1cae1d02b029a3decf216df689cd7ed1
Emmmmllll <gocki16@gmail.com>
2025-12-02 21:54:34 +0100

    feat: template for challenges

---
+++ src/template/challenge.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const utils = @import("utils");
+const Allocator = std.mem.Allocator;
+
+pub const main = utils.buildMain(.{
+    // .challenge1 = challenge1,
+    // .challenge2 = challenge2,
+});
+
+pub fn challenge1(alloc: Allocator, input: []const u8) !void {
+    return error.NotImplemented;
+}
+
+pub fn challenge2(alloc: Allocator, input: []const u8) !void {
+    return error.NotImplemented;
+}
